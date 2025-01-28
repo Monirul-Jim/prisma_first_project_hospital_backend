8c56585edb6594b9b452c9335d026d5631666f6c
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-28 21:44:59 +0600

    create user and admin use prisma transaction

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,9 +2,8 @@ import { Request, Response } from "express";
 import { UserServices } from "./user.services";
 
 const createAdmin = async (req: Request, res: Response) => {
-  console.log(req);
-  //   const result = await UserServices.createAdmin();
-  //   res.send(result);
+  const result = await UserServices.createAdmin(req.body);
+  res.send(result);
 };
 export const UserController = {
   createAdmin,
--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,7 +1,22 @@
-const createAdmin = async () => {
-  return {
-    message: "Admin created",
+import { PrismaClient, UserRole } from "@prisma/client";
+const prisma = new PrismaClient();
+const createAdmin = async (data: any) => {
+  console.log(data);
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
   };
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+    const createdAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+    return createdAdminData;
+  });
+  return result;
 };
 export const UserServices = {
   createAdmin,
