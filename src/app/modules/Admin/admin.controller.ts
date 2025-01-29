ec47b153c08c0caed331eeaf2141649e28e0fcce
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-29 12:57:32 +0600

    get admin and search

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,12 +2,20 @@ import { Request, Response } from "express";
 import { AdminServices } from "./admin.services";
 
 const getAllFromDb = async (req: Request, res: Response) => {
-  const result = await AdminServices.getAllFromDb();
-  res.status(200).json({
-    success: true,
-    message: "Admin data fetched",
-    data: result,
-  });
+  try {
+    const result = await AdminServices.getAllFromDb(req.query);
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || "Something went wrong",
+      error: err,
+    });
+  }
 };
 export const AdminController = {
   getAllFromDb,
--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -1,7 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-const getAllFromDb = async () => {
-  const result = await prisma.admin.findMany();
+const getAllFromDb = async (params: any) => {
+  console.log({ params });
+  const result = await prisma.admin.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 export const AdminServices = {
