b6b8435c4e6e8f7b7abb686354e692883c331bb1
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-29 12:44:49 +0600

    get admin by controller and services

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-const getAllAdmin = async (req: Request, res: Response) => {
-  const result = await prisma.admin.findMany();
+import { AdminServices } from "./admin.services";
+
+const getAllFromDb = async (req: Request, res: Response) => {
+  const result = await AdminServices.getAllFromDb();
   res.status(200).json({
     success: true,
     message: "Admin data fetched",
     data: result,
   });
+};
+export const AdminController = {
+  getAllFromDb,
 };
--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
+import { AdminController } from "./admin.controller";
 const router = express.Router();
 
-router.get("/");
+router.get("/", AdminController.getAllFromDb);
 export const AdminRoutes = router;
