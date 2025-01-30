6b61114f685e2090fa0efbdb36567e015d8f91f3
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-30 18:47:47 +0600

    Creating Reusable Functions for Pagination and Sorting

--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -1,9 +1,28 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { adminSearchableFields } from "./admin.constant";
 const prisma = new PrismaClient();
+const calculatePagination = (options: {
+  page?: number;
+  limit?: number;
+  sortOrder?: string;
+  sortBy?: string;
+}) => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 1;
+  const skip: number = (Number(page) - 1) * limit;
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
 const getAllFromDb = async (params: any, options: any) => {
   const { searchTerm, ...filterData } = params;
-  const { limit, page } = options;
+  const { limit, page, skip } = calculatePagination(options);
   const andConditions: Prisma.AdminWhereInput[] = [];
 
   if (params.searchTerm) {
@@ -28,8 +47,8 @@ const getAllFromDb = async (params: any, options: any) => {
   const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
   const result = await prisma.admin.findMany({
     where: whereConditions,
-    skip: (Number(page) - 1) * Number(limit),
-    take: Number(limit),
+    skip,
+    take: limit,
     orderBy:
       options.sortBy && options.sortOrder
         ? {
