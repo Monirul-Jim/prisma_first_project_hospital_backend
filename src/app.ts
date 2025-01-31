d52d69c8501fc7b2ad95d303383c1c601f8e25dd
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-31 22:41:48 +0600

    Configuring Routes in a File and Employing HTTP Status Codes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { UserRoutes } from "./app/modules/User/user.routes";
-import { AdminRoutes } from "./app/modules/Admin/admin.routes";
+import router from "./app/routes";
 const app: Application = express();
 
 app.use(cors());
@@ -10,6 +9,5 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
-app.use("/api/v1/user", UserRoutes);
-app.use("/api/v1/admin", AdminRoutes);
+app.use("/api/v1", router);
 export default app;
