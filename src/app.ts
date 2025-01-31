59af3395f43d9e2fbcf45aeb16783caff6de0e30
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-31 23:07:30 +0600

    implement not found api route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 
 app.use(cors());
@@ -12,4 +13,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.use("/api/v1", router);
 app.use(globalErrorHandler);
+app.use(notFound);
 export default app;
