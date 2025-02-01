5038591289e30b52db70942dc3f9aea8f615918e
Monirul Jim <monirulislamjim6640@gmail.com>
2025-02-01 21:01:44 +0600

    Implementing Request Validation Middleware: Part 2

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,12 +1,28 @@
 import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
+import { AnyZodObject, z } from "zod";
 const router = express.Router();
-const validateRequest = (req: Request, res: Response, next: NextFunction) => {
-  console.log();
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      return next();
+    } catch (err) {
+      next(err);
+    }
+  };
 };
 router.get("/", AdminController.getAllFromDb);
 router.get("/:id", AdminController.getByIdFromDb);
-router.patch("/:id", AdminController.updateIntoDb);
+router.patch("/:id", validateRequest(update), AdminController.updateIntoDb);
 router.delete("/:id", AdminController.deleteFromDb);
 router.delete("/soft/:id", AdminController.softDeleteFromDb);
 export const AdminRoutes = router;
