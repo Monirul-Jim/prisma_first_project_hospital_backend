5d2500e2df06291d9f96674c5d991426ad8f5387
Monirul Jim <monirulislamjim6640@gmail.com>
2025-02-01 21:05:45 +0600

    make validate requeast file and admin validation file for better organize

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,28 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
-import { AnyZodObject, z } from "zod";
+import validateRequest from "../../middlewares/validateRequest";
+import { AdminValidationSchema } from "./admin.validations";
 const router = express.Router();
-const update = z.object({
-  body: z.object({
-    name: z.string().optional(),
-    contactNumber: z.string().optional(),
-  }),
-});
-const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body: req.body,
-      });
-      return next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
+
 router.get("/", AdminController.getAllFromDb);
 router.get("/:id", AdminController.getByIdFromDb);
-router.patch("/:id", validateRequest(update), AdminController.updateIntoDb);
+router.patch(
+  "/:id",
+  validateRequest(AdminValidationSchema.update),
+  AdminController.updateIntoDb
+);
 router.delete("/:id", AdminController.deleteFromDb);
 router.delete("/soft/:id", AdminController.softDeleteFromDb);
 export const AdminRoutes = router;
