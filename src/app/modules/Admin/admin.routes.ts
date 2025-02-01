6293b176c351819d199b9c89ee16dd04839824ae
Monirul Jim <monirulislamjim6640@gmail.com>
2025-02-01 20:50:13 +0600

    Implementing Request Validation Middleware: Part 1

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
 const router = express.Router();
-
+const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+  console.log();
+};
 router.get("/", AdminController.getAllFromDb);
 router.get("/:id", AdminController.getByIdFromDb);
 router.patch("/:id", AdminController.updateIntoDb);
